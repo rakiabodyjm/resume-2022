73006ac951cc70c8d668d79a01a8d5b04f1e6695
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-08-02 03:34:53 +0800

    GlobalContainer hotfix wrong import

--- src/components/common/GlobalContainer.tsx
+++ src/components/common/GlobalContainer.tsx
@@ -1,20 +1,22 @@
 import { ContainerProps } from "@mui/material";
-import { Container } from "@mui/system";
+import { Container } from "@mui/material";
 
 export default function GlobalContainer({
   children,
+  sx,
   ...restProps
 }: ContainerProps) {
   return (
     <Container
+      disableGutters
       sx={{
         p: {
           xs: 1,
           sm: 2,
         },
-        ...restProps?.sx,
+        // ...(restProps?.sx && restProps?.sx),
+        ...sx,
       }}
-      disableGutters
       {...restProps}
     >
       {children}
