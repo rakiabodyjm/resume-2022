d08e3748ea498abf40d1e5c8099827c1c46d63a3
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-08-08 23:41:49 +0800

    graphql API nextjs with enabled cors

---
+++ pages/api/graphql.ts
@@ -0,0 +1,49 @@
+import { ApolloServer } from "apollo-server-micro";
+import { NextApiRequest, NextApiResponse, NextConfig } from "next";
+import resolvers from "src/graphql/resolvers";
+import { typeDefs } from "src/graphql/schema";
+// import Cors from "micro-cors";
+import Cors from "cors";
+
+const cors = Cors();
+
+const apolloServer = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+const startServer = apolloServer.start();
+
+// export default cors(async function handler(req, res) {
+//   await startServer;
+
+//   await apolloServer.createHandler({
+//     path: "/api/graphql",
+//   })(req, res);
+// });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await startServer;
+    cors(req, res, (result: any) => {
+      console.log("result", result);
+      if (result instanceof Error) {
+        throw result;
+      }
+    });
+
+    apolloServer.createHandler({
+      path: "/api/graphql",
+    })(req, res);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
