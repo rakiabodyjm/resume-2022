c628a4eb27b1ff6c375f60b0d8face0913b397e8
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-08-11 21:51:09 +0800

    profile GraphQL resolver

--- src/graphql/resolvers/profile.ts
+++ src/graphql/resolvers/profile.ts
@@ -1,61 +1,67 @@
 export type GenericInformation = {
-  title: string;
-  description: string;
-  iconString: string;
-};
+  title: string
+  description: string
+  iconString: string
+}
 export type PersonalInformation = {
-  name: string;
-  summary: (string | string[])[];
-  title: string;
-  genericData: GenericInformation[];
-  profiles: GenericInformation[];
-};
+  name: string
+  summary: (string | string[])[] | string
+  title: string
+  genericData: GenericInformation[]
+  profiles: GenericInformation[]
+}
 
 const personalInformation: PersonalInformation = {
-  name: "Janeo Miguel Tiongson",
-  title: "Lead Software Engineer",
+  name: 'Janeo Miguel Tiongson',
+  title: 'Lead Software Engineer',
+  // summary: [
+  //   "Passionate and process driven Software Engineer, committed to balance between Developer and Customer Experience. I'm highly active in Development Communities to keep myself up to date to new innovations and technology releases. I make useful libraries as a hobby and ",
+  //   "With hands-on experience in Software Engineering and Project Management, Development Operations, Database Management and Software Architecture. With notable projects in:",
+  //   [
+  //     "Enterprise level Financial Technology Systems ( E-Wallet, Multilevel Marketing, Payment Processing and Analytics )",
+  //     "Movie Streaming System ( Movie and TV Shows Database and File Servers, Movie Transmuxing and Transcoding, Streaming with Adaptive Http Live Streaming and some other prototypes )",
+  //     "Open Source Projects ( React Page Mailer, NFT Blockchain Game Monitoring App, GitHub Autocommit )",
+  //     "Boilerplate Repositories and Utility Libraries for Web Client and Server Frameworks",
+  //   ],
+  // ],
   summary: [
-    "Passionate and progress driven Software Engineer, committed to balance between Developer and Customer Experience, highly active in Development Communities like React.js PH and Next.js and always keeps up to date to new innovations and technology releases",
-    "With hands-on experience in Software Engineering and Project Management, Development Operations, Database Management and Software Architecture. With notable projects in:",
-    [
-      "Enterprise level Financial Technology Systems ( E-Wallet, Multilevel Marketing, Payment Processing and Analytics )",
-      "Movie Streaming System ( Movie and TV Shows Database and File Servers, Movie Transmuxing and Transcoding, Streaming with Adaptive Http Live Streaming and some other prototypes )",
-      "Open Source Projects ( E-Mail Marketing Mailer, Blockchain Game Monitoring App )",
-      "Boilerplate Repositories and Utility Libraries for Web Client and Server Frameworks",
-    ],
+    'Passionate, meticulous and process-driven Software Engineer with 5 years of progressive experience in modern web and server technologies across diverse industries.',
+    'Equipped with backgrounds in System Administration, Frontend and Backend Engineering, Database Management and Design, Software Architecture, Development Operations and Project Management.',
+    'Quickly learns, masters and introduces new innovations and technologies to provide better solutions to technical challenges.',
+    'Engages in development communities and provides insights, mentoring and training to others.',
   ],
-
+  // hobbies: ["Coding", "Drone Pilot"],
   genericData: [
     {
-      title: "Email",
-      description: "rakiabodyjm@gmail.com",
-      iconString: "AlternateEmail",
+      title: 'Email',
+      description: 'rakiabodyjm@gmail.com',
+      iconString: 'Email',
     },
     {
-      title: "Phone Number",
-      description: "+63 949 846 0475",
-      iconString: "SmartphoneOutlined",
+      title: 'Phone',
+      description: '+63 949 846 0475',
+      iconString: 'SmartphoneOutlined',
     },
     {
-      title: "Location",
-      description: "Quezon City, PH",
-      iconString: "LocationOnOutlined",
+      title: 'Location',
+      description: 'Quezon City, PH',
+      iconString: 'LocationOn',
     },
   ],
   profiles: [
     {
-      title: "GitHub",
-      description: "github.com/rakiabodyjm",
-      iconString: "github",
+      title: 'GitHub',
+      description: 'github.com/rakiabodyjm',
+      iconString: 'GitHub',
     },
     {
-      title: "LinkedIn",
-      description: "linkedin.com/in/janeomigueltiongson",
-      iconString: "linkedin",
+      title: 'LinkedIn',
+      description: 'linkedin.com/in/janeomigueltiongson',
+      iconString: 'LinkedIn',
     },
   ],
-};
+}
 
 export function getProfile(): PersonalInformation {
-  return personalInformation;
+  return personalInformation
 }
