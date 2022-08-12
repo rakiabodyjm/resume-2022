92b6fe97a78f5ed21294788a605911192a726583
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-08-12 21:52:01 +0800

    GraphQL root resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,33 +1,34 @@
-import { Config } from "apollo-server-micro";
-import { GraphQLScalarType } from "graphql";
-import { getProfile } from "src/graphql/resolvers/profile";
-import { getSkills } from "src/graphql/resolvers/skill";
-import { getTimelines } from "src/graphql/resolvers/timeline";
+import { Config } from 'apollo-server-micro'
+import { getCertificates } from 'src/graphql/resolvers/certificates'
+import { getDegrees } from 'src/graphql/resolvers/degrees'
+import { getPersonalProjects } from 'src/graphql/resolvers/personal-project'
+import { getProfile, PersonalInformation } from 'src/graphql/resolvers/profile'
+import { getRefinedSkills, getSkills } from 'src/graphql/resolvers/skill'
+import { getTimelines } from 'src/graphql/resolvers/timeline'
 
-const resolvers: Config<any>["resolvers"] = {
+const resolvers: Config<any>['resolvers'] = {
   Query: {
     timelines: getTimelines,
     skills: getSkills,
     profile: getProfile,
+    skillsv2: getRefinedSkills,
+    personal_projects: getPersonalProjects,
+    certificates: getCertificates,
+    degrees: getDegrees,
   },
   ProfileSummary: {
-    name: "ProfileSummary",
-    description: "Profile Summary with list",
-    serialize(value) {
-      console.log("valueSerialize", value);
-      // return (value as string)?.toString();
-      return JSON.stringify(value as string);
+    name: 'ProfileSummary',
+    description: 'Profile Summary with list',
+    serialize(value: PersonalInformation['summary']) {
+      return JSON.stringify(value as string)
     },
-    parseValue(value) {
-      console.log("parseValue", value);
-
-      return JSON.stringify(value as string);
+    parseValue(value: string) {
+      return JSON.parse(value)
     },
-    parseLiteral(ast) {
-      console.log("parseLiteral", ast);
-      return JSON.parse(ast as string);
+    parseLiteral(ast: string) {
+      return JSON.parse(ast as string)
     },
   },
-};
+}
 
-export default resolvers;
+export default resolvers
