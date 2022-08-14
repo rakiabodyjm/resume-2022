dcdb67131c5453ecd1618d8c405c731317d439a3
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-08-14 15:25:16 +0800

    Add resolvers into root resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,8 +1,33 @@
-import getTimelines from "src/graphql/resolvers/timeline";
+import { Config } from "apollo-server-micro";
+import { GraphQLScalarType } from "graphql";
+import { getProfile } from "src/graphql/resolvers/profile";
+import { getSkills } from "src/graphql/resolvers/skill";
+import { getTimelines } from "src/graphql/resolvers/timeline";
 
-const resolvers = {
+const resolvers: Config<any>["resolvers"] = {
   Query: {
     timelines: getTimelines,
+    skills: getSkills,
+    profile: getProfile,
+  },
+  ProfileSummary: {
+    name: "ProfileSummary",
+    description: "Profile Summary with list",
+    serialize(value) {
+      console.log("valueSerialize", value);
+      // return (value as string)?.toString();
+      return JSON.stringify(value as string);
+    },
+    parseValue(value) {
+      console.log("parseValue", value);
+
+      return JSON.stringify(value as string);
+    },
+    parseLiteral(ast) {
+      console.log("parseLiteral", ast);
+      return JSON.parse(ast as string);
+    },
   },
 };
+
 export default resolvers;
