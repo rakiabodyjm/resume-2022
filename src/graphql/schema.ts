3b1b7356bcfdd30b300f5e6fc4d4dbf95cfaecd9
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-08-15 15:25:49 +0800

    Create Custom Scalar ProfileSummary

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,25 @@
 import { gql } from "apollo-server-micro";
 
 export const typeDefs = gql`
+  scalar ProfileSummary
   type Query {
     timelines: [TimelineEntry]
+    skills: Techstacks
+    profile: Profile
+  }
+
+  type Profile {
+    name: String
+    title: String
+    summary: ProfileSummary
+    genericData: [GenericProfileData]
+    profiles: [GenericProfileData]
+  }
+
+  type GenericProfileData {
+    title: String
+    description: String
+    iconString: String
   }
 
   type ProjectEntry {
@@ -17,5 +34,28 @@ export const typeDefs = gql`
     company: String
     date: String
     projects: [ProjectEntry]
+  }
+
+  type Techstacks {
+    languages: [String]
+    orm: [String]
+    uiLibraries: [String]
+    dataLib: [String]
+    microServices: [String]
+    saas: [String]
+    dbms: [String]
+    auth: [String]
+    testing: [String]
+    otherNode: [String]
+    tools: [String]
+    environments: [String]
+    webServer: [String]
+    otherLibs: [String]
+  }
+
+  type GenericInformation {
+    title: String
+    description: String
+    icon: String
   }
 `;
