06b16f56ff4c995b4ce74a20b6cc45e9c9efa9a3
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-08-18 15:28:33 +0800

    Initialize Technologies component for framer

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,17 +1,52 @@
-import { Box, Typography } from "@mui/material";
+/* eslint-disable react/display-name */
+import {
+  Box,
+  Typography as MuiTypography,
+  TypographyProps,
+} from "@mui/material";
 import techStacks from "../data/tech-stacks";
+import { motion } from "framer-motion";
+import { ForwardedRef, forwardRef, RefObject, useRef, useState } from "react";
+
+const ForwardRefMuiTypography = forwardRef(
+  (props: TypographyProps, ref: ForwardedRef<HTMLParamElement>) => (
+    <MuiTypography ref={ref} {...props}>
+      {props?.children}
+    </MuiTypography>
+  )
+);
+const Typography = motion(ForwardRefMuiTypography);
 
 export default function Technologies() {
+  const ContainerRef = useRef<null | HTMLDivElement>(null);
   return (
     <Box
+      ref={ContainerRef}
       sx={{
         display: "flex",
         flexWrap: "wrap",
         gap: 1,
       }}
     >
-      {techStacks.flat().map((tech) => (
+      {techStacks.flat().map((tech, index) => (
         <Typography
+          // draggable
+          // drag
+          // onDragOver={() => {
+          //   console.log("being dropped to ", index);
+          // }}
+          // onDrag={() => {
+          //   setActiveDrag(index);
+          //   console.log("being dragged ", index);
+          // }}
+          // onDragEnd={() => {
+          //   console.log("drag end");
+
+          // }}
+          // whileDrag={{
+          //   skewX: 4,
+          // }}
+          // dragConstraints={ContainerRef}
           key={tech}
           sx={{
             padding: "2px 8px",
