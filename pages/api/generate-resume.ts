2fa8f26a1a484c295532bc74838ca6bf7083de64
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-08-21 21:43:38 +0800

    Add generate-resume logic and api endpoint

---
+++ pages/api/generate-resume.ts
@@ -0,0 +1,89 @@
+import { readFileSync } from 'fs'
+import { NextApiResponse } from 'next'
+import { NextApiRequest } from 'next'
+import { resolve } from 'path'
+import puppeteer from 'puppeteer'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const browser = await puppeteer.launch({
+    headless: true,
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto('http://localhost:3000/resume2', {
+    waitUntil: 'networkidle0',
+    // timeout: 30000,
+  })
+
+  await page.setViewport({
+    width: 1227,
+    height: 841,
+  })
+
+  //   await autoScroll(page)
+  await page.evaluate(
+    () =>
+      new Promise((resolve) => {
+        var scrollTop = -1
+        const interval = setInterval(() => {
+          window.scrollBy(0, 100)
+          if (document.documentElement.scrollTop !== scrollTop) {
+            scrollTop = document.documentElement.scrollTop
+            return
+          }
+          clearInterval(interval)
+          resolve(null)
+        }, 10)
+      })
+  )
+
+  const path = resolve(process.cwd(), 'public', 'TIONGSON_JANEOMIGUEL.pdf')
+
+  const pdf = await page.pdf({
+    format: 'a4',
+    printBackground: true,
+    preferCSSPageSize: true,
+    // path: resolve(process.cwd(), 'public', 'TIONGSON_JANEOMIGUEL.pdf'),
+    path,
+  })
+
+  //   const pdf = await page.screenshot({
+  //     fullPage: true,
+  //     type: 'jpeg',
+  //     quality: 100,
+  //   })
+
+  await browser.close()
+
+  // res.pipe()
+  res.setHeader('Content-Type', 'application/pdf')
+  //   res.setHeader(
+  //     'Content-Disposition',
+  //     'attachment;filename=JaneoMiguelTiongsonCV.pdf'
+  //   )
+  res.send(readFileSync(path))
+  //   res.pipe(pdf)
+}
+
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0
+      var distance = 100
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight
+        window.scrollBy(0, distance)
+        totalHeight += distance
+
+        if (totalHeight >= scrollHeight - window.innerHeight) {
+          clearInterval(timer)
+          resolve(null)
+        }
+      }, 200)
+    })
+  })
+}
