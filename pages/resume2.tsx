9eae0c1d499d74d4409832a910d7dd896ebd320d
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-08-21 21:43:15 +0800

    Add resume2 page

--- pages/resume2.tsx
+++ pages/resume2.tsx
@@ -1,57 +1,14 @@
 import { gql, useQuery } from "@apollo/client";
 import { Grid, styled, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import SideBar from "src/components/common/SideBar";
 import HeaderGraphQL from "src/components/HeaderGraphQL";
+import ResumeTemplate1 from "src/components/resume-template-1";
 import { PersonalInformation } from "src/graphql/resolvers/profile";
 
-const GET_PROFILE = gql`
-  query GetProfile {
-    profile {
-      name
-      title
-      summary
-      genericData {
-        title
-        description
-        iconString
-      }
-      profiles {
-        title
-        description
-        iconString
-      }
-    }
-  }
-`;
 const ResumeGrid = styled(Grid)((theme) => ({}));
 export default function Resume2({}) {
-  const { loading, error, data } = useQuery<{ profile: PersonalInformation }>(
-    GET_PROFILE
+  return (
+    <>
+      <ResumeTemplate1 />
+    </>
   );
-
-  if (loading) {
-    return (
-      <Typography
-        noWrap
-        variant="h4"
-        sx={{
-          margin: "auto",
-        }}
-      >
-        Loading...
-      </Typography>
-    );
-  }
-  if (data) {
-    return (
-      <>
-        {/* <ResumeGrid container spacing={2}>
-        <SideBar sx={{}} />
-      </ResumeGrid> */}
-        <HeaderGraphQL headerDetails={data.profile} />
-        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      </>
-    );
-  }
 }
