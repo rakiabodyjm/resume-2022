29ee386fb77e9bacc8da4256bf8beaccc4fc877c
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-08-25 21:48:55 +0800

    Updated degrees data

---
+++ src/data/degrees.ts
@@ -0,0 +1,15 @@
+export type Degrees = {
+  name: string
+  date: string
+}
+
+export const degrees = [
+  {
+    name: 'BS in Information Technology',
+    date: 'April 2016 - December 2019',
+  },
+  {
+    name: 'BS in Civil Engineering ( Undergrad )',
+    date: 'June 2012 - March 2016',
+  },
+]
