60e722e7de5ee1062a1bfbebded35d2e087b5f40
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-08-25 21:48:02 +0800

    ResumeTemplate1 Timeline component

---
+++ src/components/resume-template-1/Timeline.tsx
@@ -0,0 +1,72 @@
+import { TimelineEntry } from '@data/timeline'
+import { Box, BoxProps, SxProps, Theme, Typography } from '@mui/material'
+import { Fragment } from 'react'
+
+export default function Timeline({
+  sx,
+  timeLines,
+  ...restProps
+}: {
+  timeLines: TimelineEntry[]
+  sx?: SxProps<Theme>
+} & BoxProps) {
+  return (
+    <Box sx={sx} className="bordered" {...restProps}>
+      <Typography className="section-caption">Employment</Typography>
+
+      {timeLines.map(({ position, company, date, projects }) => (
+        <Box
+          className="timeline-entry-container"
+          key={position + company + date}
+        >
+          <Typography className="position ">{position}</Typography>
+          <Typography className="company">{company}</Typography>
+
+          <Typography className="date">{date}</Typography>
+
+          {projects.map(
+            ({
+              name,
+              responsibilities,
+              summarizedResponsibilities,
+              description,
+              technologies,
+            }) => (
+              <Box
+                className="projects-container sub-bordered"
+                key={name + technologies.toString()}
+              >
+                <Typography className="name">{name}</Typography>
+                <Typography className="description">{description}</Typography>
+
+                <Box component="ul" className="responsibilities-container">
+                  {summarizedResponsibilities?.map((task) => (
+                    <Typography
+                      component="li"
+                      className="responsibility"
+                      key={task}
+                    >
+                      {task}
+                    </Typography>
+                  ))}
+                </Box>
+
+                <Box className="technologies-container">
+                  <Typography className="technologies-title">
+                    Technologies:
+                  </Typography>
+
+                  {technologies.map((tech) => (
+                    <Typography className="technology commad" key={tech}>
+                      {tech}
+                    </Typography>
+                  ))}
+                </Box>
+              </Box>
+            )
+          )}
+        </Box>
+      ))}
+    </Box>
+  )
+}
